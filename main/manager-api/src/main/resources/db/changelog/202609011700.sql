af48bcca89dc59c979d7da0753ba452d730b2674
tobatha <tabatha2005@gmail.com>
2026-09-10 11:30:05 +0800

    名称更新

--- main/manager-api/src/main/resources/db/changelog/202609011700.sql
+++ main/manager-api/src/main/resources/db/changelog/202609011700.sql
@@ -1,3 +1,2 @@
 ALTER TABLE `ai_agent`
-ADD COLUMN `additional_prompt` LONGTEXT NULL COMMENT '角色个性特色' AFTER `system_prompt`;
-
+ADD COLUMN `additional_prompt` LONGTEXT NULL COMMENT '角色附加提示词' AFTER `system_prompt`;
